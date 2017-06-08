1076e8d140f709d69c48f8620ab95c9d1425b7c5
Michael Kelly <mkelly@mozilla.com>
2017-06-08 13:55:25 -0700

    Use OS.File directly.

--- bootstrap.js
+++ bootstrap.js
@@ -1,20 +1,30 @@
 const {utils: Cu} = Components;
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-XPCOMUtils.defineLazyModuleGetter(this, "JSONFile", "resource://gre/modules/JSONFile.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "OS", "resource://gre/modules/osfile.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "Log", "resource://gre/modules/Log.jsm");
 
 this.install = function() {};
 
 this.startup = async function() {
-  const file = new JSONFile({path: 'resource://jsonfile-example/data.json'});
-  await file.load();
-
   const logger = Log.repository.getLogger('jsonfile-example');
   logger.addAppender(new Log.ConsoleAppender(new Log.BasicFormatter()));
   logger.level = 0;
-  logger.debug('Loaded JSON file');
-  logger.debug(file.data);
+
+  logger.debug('Loading JSON file');
+  try {
+    const bytes = await OS.File.read('resource://jsonfile-example/data.json', {});
+
+    logger.debug('Parsing JSON file');
+    const decoder = new TextDecoder();
+    const data = JSON.parse(decoder.decode(bytes));
+
+    logger.debug('Loaded JSON file');
+    logger.debug(data);
+  } catch (error) {
+    logger.error('Failed to load JSON file: ${error.message}');
+    logger.error(error);
+  }
 };
 
 this.shutdown = function() {};
